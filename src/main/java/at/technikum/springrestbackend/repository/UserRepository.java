e9ea0db68296443ba8faf38c6dc90c961d11a98f
e12823 <wi23b128@technikum-wien.at>
2025-12-02 19:24:48 +0100

    feat: Implement AuthService

--- src/main/java/at/technikum/springrestbackend/repository/UserRepository.java
+++ src/main/java/at/technikum/springrestbackend/repository/UserRepository.java
@@ -8,7 +8,10 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsByEmail(String email);
+
     boolean existsByUsername(String username);
+
     Optional<User> findByEmail(String email);
 }
